b26eeba6fcf823004fcb5c93a1014eadeed74cc2
rafa <rmxvgit@gmail.com>
2025-03-28 11:37:34 -0300

    crud do hospital pronto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LaudoModule } from './laudo/laudo.module';
-import { HosiptalModule } from './hosiptal/hosiptal.module';
 import { HospitalModule } from './hospital/hospital.module';
 
 @Module({
-  imports: [LaudoModule, HosiptalModule, HospitalModule],
+  imports: [LaudoModule, HospitalModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HospitalService } from './hospital.service';
 import { HospitalController } from './hospital.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [HospitalController],
-  providers: [HospitalService],
+  providers: [HospitalService, PrismaService],
 })
 export class HospitalModule {}
