3f487901030ae57318f66d2ac4a8c08eded74b52
rmxvgit <rmxvgit@gmail.com>
2025-07-17 19:43:31 -0300

    deleção de hospital está funcionando corretamente

--- src/hospital/hospital.service.ts
+++ src/hospital/hospital.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { CreateHospitalDto } from './dto/create-hospital.dto';
 //import { UpdateHospitalDto } from './dto/update-hospital.dto';
 import { PrismaService } from 'src/prisma.service';
+import { unlinkSync } from 'node:fs';
+import { join } from 'node:path/posix';
+import { LAUDOS_DIR } from 'src/project_structure/dirs';
 
 @Injectable()
 export class HospitalService {
@@ -34,6 +37,16 @@ export class HospitalService {
   */
 
   async remove(cnes: number) {
+    const laudos = await this.prisma.laudo.findMany({ where: { cnes: cnes } });
+
+    for (const laudo of laudos) {
+      await this.prisma.laudo.delete({ where: { id: laudo.id } });
+      unlinkSync(join(LAUDOS_DIR, `${laudo.file_name}.pdf`));
+      unlinkSync(join(LAUDOS_DIR, `${laudo.file_name}.tex`));
+      unlinkSync(join(LAUDOS_DIR, `PA${laudo.file_name}.csv`));
+      unlinkSync(join(LAUDOS_DIR, `SP${laudo.file_name}.csv`));
+    }
+
     return await this.prisma.hospital.delete({
       where: { cnes: cnes },
     });
