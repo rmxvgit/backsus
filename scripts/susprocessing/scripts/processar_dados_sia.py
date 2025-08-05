b24d72b4d0dcd60c6f17c919f1b6e4ea61dfe6e9
rafa <rmxvgit@gmail.com>
2025-08-05 15:15:49 -0300

    troca de juros compostos por juros simples

--- scripts/susprocessing/scripts/processar_dados_sia.py
+++ scripts/susprocessing/scripts/processar_dados_sia.py
@@ -12,7 +12,7 @@ from tempo import Tdata
 # python3 processar_dados.py <sourceFile> <CNES> <destinationFIle>
 
 # essa tabela vai de maio do ano 2000 até novembro de 2021
-tabela_ipcae = [0.09,0.08,0.78,1.99,0.45,0.18,0.17,0.6,0.63,0.5,0.36,0.5,0.49,0.38,0.94,1.18,0.38,0.37,0.99,0.55,0.62,0.44,0.4,0.78,0.42,0.33,0.77,1,0.62,0.9,2.08,3.05,1.98,2.19,1.14,1.14,0.85,0.22,-0.18,0.27,0.57,0.66,0.17,0.46,0.68,0.9,0.4,0.21,0.54,0.56,0.93,0.79,0.49,0.32,0.63,0.84,0.68,0.74,0.35,0.74,0.83,0.12,0.11,0.28,0.16,0.56,0.78,0.38,0.51,0.52,0.37,0.17,0.27,-0.15,-0.02,0.19,0.05,0.29,0.37,0.35,0.52,0.46,0.41,0.22,0.26,0.29,0.24,0.42,0.29,0.24,0.23,0.7,0.7,0.64,0.23,0.59,0.56,0.9,0.63,0.35,0.26,0.3,0.49,0.29,0.4,0.63,0.11,0.36,0.59,0.38,0.22,0.23,0.19,0.18,0.44,0.38,0.52,0.94,0.55,0.48,0.63,0.19,-0.09,-0.05,0.31,0.62,0.86,0.69,0.76,0.97,0.6,0.77,0.7,0.23,0.1,0.27,0.53,0.42,0.46,0.56,0.65,0.53,0.25,0.43,0.51,0.18,0.33,0.39,0.48,0.65,0.54,0.69,0.88,0.68,0.49,0.51,0.46,0.38,0.07,0.16,0.27,0.48,0.57,0.75,0.67,0.7,0.73,0.78,0.58,0.47,0.17,0.14,0.39,0.48,0.38,0.79,0.89,1.33,1.24,1.07,0.6,0.99,0.59,0.43,0.39,0.66,0.85,1.18,0.92,1.42,0.43,0.51,0.86,0.4,0.54,0.45,0.23,0.19,0.26,0.19,0.31,0.54,0.15,0.21,0.24,0.16,-0.18,0.35,0.11,0.34,0.32,0.35,0.39,0.38,0.1,0.21,0.14,1.11,0.64,0.13,0.09,0.58,0.19,-0.16,0.3,0.34,0.54,0.72,0.35,0.06,0.09,0.08,0.09,0.09,0.14,1.05,0.71,0.22,0.02,-0.01,-0.59,0.02,0.3,0.23,0.45,0.94,0.81,1.06,0.78,0.48,0.93,0.6,0.44,0.83,0.72,0.89,1.14,1.2,1.17]
+tabela_ipcae: list[float] = [0.09,0.08,0.78,1.99,0.45,0.18,0.17,0.6,0.63,0.5,0.36,0.5,0.49,0.38,0.94,1.18,0.38,0.37,0.99,0.55,0.62,0.44,0.4,0.78,0.42,0.33,0.77,1,0.62,0.9,2.08,3.05,1.98,2.19,1.14,1.14,0.85,0.22,-0.18,0.27,0.57,0.66,0.17,0.46,0.68,0.9,0.4,0.21,0.54,0.56,0.93,0.79,0.49,0.32,0.63,0.84,0.68,0.74,0.35,0.74,0.83,0.12,0.11,0.28,0.16,0.56,0.78,0.38,0.51,0.52,0.37,0.17,0.27,-0.15,-0.02,0.19,0.05,0.29,0.37,0.35,0.52,0.46,0.41,0.22,0.26,0.29,0.24,0.42,0.29,0.24,0.23,0.7,0.7,0.64,0.23,0.59,0.56,0.9,0.63,0.35,0.26,0.3,0.49,0.29,0.4,0.63,0.11,0.36,0.59,0.38,0.22,0.23,0.19,0.18,0.44,0.38,0.52,0.94,0.55,0.48,0.63,0.19,-0.09,-0.05,0.31,0.62,0.86,0.69,0.76,0.97,0.6,0.77,0.7,0.23,0.1,0.27,0.53,0.42,0.46,0.56,0.65,0.53,0.25,0.43,0.51,0.18,0.33,0.39,0.48,0.65,0.54,0.69,0.88,0.68,0.49,0.51,0.46,0.38,0.07,0.16,0.27,0.48,0.57,0.75,0.67,0.7,0.73,0.78,0.58,0.47,0.17,0.14,0.39,0.48,0.38,0.79,0.89,1.33,1.24,1.07,0.6,0.99,0.59,0.43,0.39,0.66,0.85,1.18,0.92,1.42,0.43,0.51,0.86,0.4,0.54,0.45,0.23,0.19,0.26,0.19,0.31,0.54,0.15,0.21,0.24,0.16,-0.18,0.35,0.11,0.34,0.32,0.35,0.39,0.38,0.1,0.21,0.14,1.11,0.64,0.13,0.09,0.58,0.19,-0.16,0.3,0.34,0.54,0.72,0.35,0.06,0.09,0.08,0.09,0.09,0.14,1.05,0.71,0.22,0.02,-0.01,-0.59,0.02,0.3,0.23,0.45,0.94,0.81,1.06,0.78,0.48,0.93,0.6,0.44,0.83,0.72,0.89,1.14,1.2,1.17]
 
 def get_path(*parts):
     """Constrói caminhos absolutos de forma confiável"""
@@ -43,8 +43,9 @@ def correcao_ipcae(data_inicio: Tdata, data_fim: Tdata):  # noqa: E501
     if posicao_relativa_do_fim >= len(tabela_ipcae):
         posicao_relativa_do_fim = len(tabela_ipcae)-1
 
+    correcao_list = [(x/100.0) for x in tabela_ipcae[posicao_relativa_do_inicio:(posicao_relativa_do_fim+1)]]
 
-    indice_a_se_aplicar = m.prod([(x/100)+1 for x in tabela_ipcae[posicao_relativa_do_inicio:(posicao_relativa_do_fim+1)]])
+    indice_a_se_aplicar = sum(correcao_list)+1.0
 
     return indice_a_se_aplicar
 
@@ -96,7 +97,7 @@ def CalculaSelic(data_inicio: Tdata, data_fim: Tdata, selic_array: list[float]):
     if posicao_do_fim_na_array >= len(selic_array):
         posicao_do_fim_na_array = len(selic_array)-1
 
-    return m.prod(selic_array[posicao_do_inicio_na_array:posicao_do_fim_na_array+1])
+    return sum([x-1.0 for x in selic_array[posicao_do_inicio_na_array:posicao_do_fim_na_array+1]]) + 1.0
 
 
 def correcao_absoluta(data_inicio: Tdata, data_fim: Tdata, selic_arr: list[float]):
@@ -136,10 +137,10 @@ def calcular_tunep(df: pd.DataFrame) -> pd.DataFrame:
     df_tunep = pd.read_csv(get_path('dados', 'tabela_tunep_mais_origem.csv'))
     df_tunep['TP_PROCEDIMENTO'] = df_tunep['TP_PROCEDIMENTO'] == 'A'
     df_tunep['CO_PROCEDIMENTO'] = df_tunep['CO_PROCEDIMENTO'].astype(str)
-    
+
     df['CO_PROCEDIMENTO'] = df['CO_PROCEDIMENTO'].astype(str)
     df = pd.merge(df, df_tunep, on='CO_PROCEDIMENTO', how='left').fillna(0)
-    
+
     df['ValorTUNEP'] = pd.to_numeric(df['ValorTUNEP'].astype(str).str.replace(',', '.'), errors='coerce').fillna(0)
     df['TUNEP'] = df['PA_QTDAPR'] * df['ValorTUNEP']
     return df
@@ -157,35 +158,35 @@ def adicionar_base_sus(df: pd.DataFrame) -> pd.DataFrame:
 def formatar_colunas(df: pd.DataFrame) -> pd.DataFrame:
     # Formatação do código do procedimento
     df['CO_PROCEDIMENTO'] = df['CO_PROCEDIMENTO'].astype(str).str.zfill(10)
-    df['CO_PROCEDIMENTO'] = (df['CO_PROCEDIMENTO'].str[:2] + "." + 
-                            df['CO_PROCEDIMENTO'].str[2:4] + "." + 
-                            df['CO_PROCEDIMENTO'].str[4:6] + "." + 
-                            df['CO_PROCEDIMENTO'].str[6:9] + "-" + 
+    df['CO_PROCEDIMENTO'] = (df['CO_PROCEDIMENTO'].str[:2] + "." +
+                            df['CO_PROCEDIMENTO'].str[2:4] + "." +
+                            df['CO_PROCEDIMENTO'].str[4:6] + "." +
+                            df['CO_PROCEDIMENTO'].str[6:9] + "-" +
                             df['CO_PROCEDIMENTO'].str[9])
     # Formatação da data
     df['PA_CMP'] = df['PA_CMP'].astype(str).str[4:] + '/' + df['PA_CMP'].astype(str).str[:4]
-    
+
     df = df[['CO_PROCEDIMENTO', 'NO_PROCEDIMENTO', 'PA_CMP', 'PA_VALPRO', 'PA_QTDAPR', 'IVR', 'TUNEP', 'correcao', 'Total', 'Base SUS']].rename(
         columns = {'CO_PROCEDIMENTO': 'Procedimentos', 'NO_PROCEDIMENTO': 'Desc. Procedimento', 'PA_CMP': 'Mês/Ano', 'PA_VALPRO': 'Valor Base (R$)', 'PA_QTDAPR': 'Qtd. Base', 'IVR': 'IVR (R$)', 'TUNEP': 'TUNEP (R$)', 'correcao' :'Correção'})
-    
+
     return df
 
 def processar_dados_csv(csv_file_path: str, output_file_path: str, data_inicio: Tdata, data_fim: Tdata, tipo_valor: str):
-    
+
     df_filt = carregar_e_preparar_dados(csv_file_path)
 
     df_sum = agregar_ordenar_dados(df_filt)
-    
+
     df_desc = mesclar_com_desc_procedimentos(df_sum)
-    
+
     if tipo_valor == 'IVR':
         df_desc = calcular_ivr(df_desc)
         df_desc["TUNEP"] = 0
-        
+
     elif tipo_valor == 'TUNEP':
         df_desc = calcular_tunep(df_desc)
         df_desc["IVR"] = 0
-        
+
     elif tipo_valor == 'Ambos':
         df_desc = calcular_ivr(df_desc)
         df_desc = calcular_tunep(df_desc)
@@ -197,15 +198,14 @@ def processar_dados_csv(csv_file_path: str, output_file_path: str, data_inicio: 
         # Caso queira valor vazio e nao zero
         # df_desc.loc[ivr_maior, "TUNEP"] = pd.NA
         # df_desc.loc[~ivr_maior, "IVR"] = pd.NA
-    
+
     df_final = aplicar_correcao(df_desc, data_inicio, data_fim)
-    
+
     df_final = adicionar_base_sus(df_final)
-    
+
     df_final = formatar_colunas(df_final)
 
     print(df_final)
 
     df_final.to_csv(f"{output_file_path}", sep=';', index=False,  encoding='utf-8-sig')
     print(f"Resultado salvo em: {output_file_path}")
-
